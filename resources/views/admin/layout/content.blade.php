0c9eb98bea5a323cc4acb682a35460ee9bc5c7db
wangjunjie <wjj900906@126.com>
2017-08-11 16:38:29 +0800

    修改模版中的url,和样式地址

--- resources/views/admin/layout/content.blade.php
+++ resources/views/admin/layout/content.blade.php
@@ -4,13 +4,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>管理员列表 </title>
     <meta name="robots" content="noindex, nofollow">
-    <link rel="shortcut icon" href="../favicon.ico" tppabs="http://dsc.com/favicon.ico" />
-    <link rel="icon" href="../animated_favicon.gif" tppabs="http://dsc.com/animated_favicon.gif" type="image/gif" />
-    <link href="/css/main.css" tppabs="http://dsc.com/admin/css/main.css" rel="stylesheet" type="text/css" />
-    <link href="/css/iconfont.css" tppabs="http://dsc.com/admin/css/iconfont.css" rel="stylesheet" type="text/css" />
-    <link href="/css/font-awesome.min.css" tppabs="http://dsc.com/admin/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="/css/perfect-scrollbar.min.css" tppabs="http://dsc.com/admin/css/perfect-scrollbar.min.css" rel="stylesheet" type="text/css">
-    <link href="/css/purebox.css" tppabs="http://dsc.com/admin/css/purebox.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="{{asset('/favicon.ico')}}" tppabs="http://dsc.com/favicon.ico" />
+    <link rel="icon" href="{{asset('/animated_favicon.gif')}}" tppabs="http://dsc.com/animated_favicon.gif" type="image/gif" />
+    <link href="{{asset('/css/main.css')}}" tppabs="http://dsc.com/admin/css/main.css" rel="stylesheet" type="text/css" />
+    <link href="{{asset('/css/iconfont.css')}}" tppabs="http://dsc.com/admin/css/iconfont.css" rel="stylesheet" type="text/css" />
+    <link href="{{asset('/css/font-awesome.min.css')}}" tppabs="http://dsc.com/admin/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{asset('/css/perfect-scrollbar.min.css')}}" tppabs="http://dsc.com/admin/css/perfect-scrollbar.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('/css/purebox.css')}}" tppabs="http://dsc.com/admin/css/purebox.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="{{asset('/js/spectrum-master/spectrum.css')}}" tppabs="http://dsc.com/admin/js/spectrum-master/spectrum.css">
     <script type="text/javascript" src="{{asset('js/jquery-1.9.1.min.js')}}"></script>
 <style>
